bdd3122fc5f228b4d3a5b936c5c9e91c27a2bcb8
Himadri Sekhar Basu <hsb10@iitbbs.ac.in>
2024-10-26 01:18:17 +0530

    Update javascripts version

--- styles/jscripts/head.js
+++ styles/jscripts/head.js
@@ -15,8 +15,8 @@ function loadFile(path, type) {
 }
 
 // Scripts
-loadFile("https://cdnjs.cloudflare.com/ajax/libs/marked/1.1.1/marked.min.js", "js");
-loadFile("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.2/highlight.min.js", "js");
+loadFile("https://cdnjs.cloudflare.com/ajax/libs/marked/4.0.1/marked.min.js", "js");
+loadFile("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.10.0/highlight.min.js", "js");
 // loadFile("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.2/languages/javascript.min.js", "js");
 loadFile("https://hsbasu.github.io/styles/jscripts/header.js", "js");
 loadFile("https://hsbasu.github.io/styles/jscripts/footer.js", "js");
@@ -31,5 +31,5 @@ loadFile("https://hsbasu.github.io/styles/css-styles/responsive_navbar.css", "cs
 loadFile("https://hsbasu.github.io/styles/css-styles/responsive_sections.css", "css");
 loadFile("https://hsbasu.github.io/styles/css-styles/slideshow.css", "css");
 loadFile("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css", "css");
-loadFile("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.2/styles/atom-one-light.min.css", "css");
+loadFile("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.10.0/styles/atom-one-light.min.css", "css");
 // loadFile("https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/4.0.0/github-markdown.min.css", "css");
