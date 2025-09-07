0e1de03208f3d37d0d2cae296c7e8a8420714b2c
Himadri Sekhar Basu <hsb10@iitbbs.ac.in>
2025-09-07 12:21:30 +0530

    Add Jquery and comments
    - Update font sizes

--- styles/jscripts/head.js
+++ styles/jscripts/head.js
@@ -1,33 +1,43 @@
+/**
+ * Dynamically loads JavaScript or CSS files into the document head.
+ * @param {string} path - URL of the file to load.
+ * @param {string} type - Either "js" for scripts or "css" for stylesheets.
+ */
 function loadFile(path, type) {
-  if (type == "js") {
-    var fileref = document.createElement("script");
-    fileref.async = false; //<-- the important part
+  let fileref;
+
+  if (type === "js") {
+    fileref = document.createElement("script");
+    fileref.async = false; // Ensure scripts are executed in order
     fileref.setAttribute("type", "text/javascript");
     fileref.setAttribute("src", path);
-  }
-  else if (type == "css") {
-    var fileref = document.createElement("link");
+  } else if (type === "css") {
+    fileref = document.createElement("link");
     fileref.setAttribute("rel", "stylesheet");
     fileref.setAttribute("type", "text/css");
     fileref.setAttribute("href", path);
   }
+
   document.getElementsByTagName("head")[0].appendChild(fileref);
 }
 
-// Scripts
+// Load core libraries first
+loadFile("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js", "js");
+
+// Load Markdown rendering and syntax highlighting libraries
 loadFile("https://cdnjs.cloudflare.com/ajax/libs/marked/4.0.1/marked.min.js", "js");
 loadFile("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.10.0/highlight.min.js", "js");
+
+// Load site-specific scripts
 loadFile("https://hsbasu.github.io/styles/jscripts/header.js", "js");
 loadFile("https://hsbasu.github.io/styles/jscripts/footer.js", "js");
 loadFile("https://hsbasu.github.io/styles/jscripts/navbar.js", "js");
-loadFile("https://hsbasu.github.io/styles/jscripts/md_html.min.js", "js"); // our markdown wrapper
-loadFile("https://hsbasu.github.io/styles/jscripts/markdown-loader.js", "js"); // Markdown loader
+loadFile("https://hsbasu.github.io/styles/jscripts/md_html.min.js", "js"); // Markdown wrapper
+loadFile("https://hsbasu.github.io/styles/jscripts/markdown-loader.js", "js"); // Markdown file loader
 loadFile("https://hsbasu.github.io/styles/jscripts/slideshow.js", "js");
 
-// Stylesheets
-loadFile("https://cdn.jsdelivr.net/gh/SH20RAJ/ShowdownCSS@main/showdown.css", "css")
-
-// GitHub-style markdown
+// Load site and third-party stylesheets
+loadFile("https://cdn.jsdelivr.net/gh/SH20RAJ/ShowdownCSS@main/showdown.css", "css");
 loadFile("https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/4.0.0/github-markdown.min.css", "css");
 loadFile("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/github.min.css", "css");
 loadFile("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css", "css");
